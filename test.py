609b3b86912d077b398de392370fb6e754322cd5
babahmed <ba21414@essex.ac.uk>
2021-10-18 22:47:37 +0100

    create test arguements

---
+++ test.py
@@ -0,0 +1,26 @@
+import unittest
+
+import Phone
+
+
+class TestPhoneBook(unittest.TestCase):
+    def test_menu(self):
+        self.assertEqual(True, True)
+
+    def test_add_contact(self):
+        self.assertEqual(True, True)
+
+    def test_list_contact(self):
+        self.assertEqual(True, True)
+
+    def test_find_contact(self):
+        self.assertEqual(True, True)
+
+    def test_modify_contact(self):
+        self.assertEqual(True, True)
+
+    def test_delete_contact(self):
+        self.assertEqual(True, True)
+
+    def test_exit_program(self):
+        self.assertEqual(True, True)
